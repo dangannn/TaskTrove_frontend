2ecf03b508aac5d4154fd3566e25a02cf32faa7a
dangannn <dn14072004@mail.ru>
2023-12-07 13:28:31 +0300

    fix: pagination fixes

--- src/components/ui/Pagination.tsx
+++ src/components/ui/Pagination.tsx
@@ -3,36 +3,36 @@ import React from 'react'
 import nextIcon from '../../assets/images/next-arrow.svg'
 import prevIcon from '../../assets/images/prev-arrow.svg'
 
-interface PaginationProps {
+interface IPaginationProps {
   prev: string
   next: string
   setData: any
 }
 
-const Pagination = (props: PaginationProps) => {
+const Pagination = ({ prev, next, setData }: IPaginationProps) => {
   return (
     <div className="mx-auto flex w-fit">
       <button
         className={`ease w-fit rounded-lg duration-300 hover:bg-blue-200 active:bg-blue-200/60 ${
-          props.prev == '' ? 'invisible' : ''
+          prev == '' ? 'invisible' : ''
         }`}
-        onClick={props.setData.bind(null, props.prev)}
+        onClick={setData.bind(null, prev)}
       >
         <img
           alt="следующая странциа"
-          className={`w-8 ${props.prev == '' ? 'invisible' : ''}`}
+          className={`w-8 ${prev == '' ? 'invisible' : ''}`}
           src={prevIcon}
         />
       </button>
       <button
         className={`ease w-fit rounded-lg duration-300 hover:bg-blue-200 active:bg-blue-200/60 ${
-          props.next == '' ? 'invisible' : ''
+          next == '' ? 'invisible' : ''
         }`}
-        onClick={props.setData.bind(null, props.next)}
+        onClick={setData.bind(null, next)}
       >
         <img
           alt="следующая странциа"
-          className={`w-8 ${props.next == '' ? 'invisible' : ''}`}
+          className={`w-8 ${next == '' ? 'invisible' : ''}`}
           src={nextIcon}
         />
       </button>
